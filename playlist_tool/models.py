1b7313ba52ae89f7b2288f5ab750b7de6d458287
aegis945 <artiom.lekincev@gmail.com>
2024-11-23 00:55:46 +0200

    small fix to spotify profile model

--- playlist_tool/models.py
+++ playlist_tool/models.py
@@ -1,8 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import User
 
 class SpotifyProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="spotify_profile")
     spotify_id = models.CharField(max_length=100, unique=True)
     display_name = models.CharField(max_length=100)
     profile_image = models.URLField(blank=True, null=True)
